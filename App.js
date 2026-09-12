f5b85a040b8f983ac98ee951448d7d39f3f67a68
Gaddamkavya98 <gaddamkavya925@gmail.com>
2026-09-12 21:23:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react'
+
+const App = () => {
+  const [data,setData] = useState([]);
+  useEffect(() => { 
+    fetch('https://api.covid19india.org/data.json').then(
+      res => res.join() 
+    ).then(jsondata => setData(jsondata.statewise))
+  },[]) 
+  return ( 
+    <div> 
+      <center>
+      <h1>INDIA COVID-19 DASHBOARD</h1>
+      <table className="table">
+        <thead className="thead-dark">
+          <tr>
+            <th>State</th>
+            <th>Confirmed</th>
+            <th>Recovered</th>
+            <th>Deaths</th>
+            <th>Active</th>
+            <th>LastUpdate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {DataTransfer.map(item => { 
+            return ( 
+              <tr>
+                <td>{item.state}</td>
+                <td>{item.confirmed}</td>
+                <td>{item.recovered}</td>
+                <td>{item.deaths}</td>
+                <td>{item.active}</td> 
+                <td>{item.lastupdatedtime}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+
+      </table>
+      </center>
+      </div>
+  )
+}
